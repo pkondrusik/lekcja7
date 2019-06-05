8ff9e19c717a5a66cec5a34ea674096e5bbedc06
pkondrusik <piotr.kondrusik@ttpsc.pl>
2019-06-05 15:56:17 +0200

    1,2,3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.*;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> listaStringow = new ArrayList<>();
+        listaStringow.add("test1");
+        listaStringow.add("test2");
+        listaStringow.add("test3");
+
+        System.out.println(listaStringow);
+
+        Set<String> imie = new TreeSet<>();
+        imie.add("set1");
+        imie.add("set2");
+        imie.add("set3");
+
+        System.out.println(imie);
+
+        Map<String,Double> mapa = new TreeMap<>();
+        mapa.put("test1", 1.0);
+        mapa.put("test2", 2.0);
+        mapa.put("test3", 3.0);
+
+        System.out.println(mapa.keySet() + "\n" + mapa.values());
+
+
     }
 }
